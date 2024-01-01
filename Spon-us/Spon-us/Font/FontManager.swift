a2137090e38edb4a8ff909fa6ab3a4ababb3c289
yubin <yubin412@naver.com>
2024-01-02 00:35:55 +0900

    [#4] 초기 개발 환경 세팅 에셋 정리, 폰트 Extension으로 추가

---
+++ Spon-us/Spon-us/Font/FontManager.swift
@@ -0,0 +1,64 @@
+//
+//  FontManager.swift
+//  Spon-us
+//
+//  Created by yubin on 1/1/24.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    // Heading
+    static let Heading01: Font = .custom("Pretendard-SemiBold", size: 28)
+    static let Heading02: Font = .custom("Pretendard-Medium", size: 28)
+    static let Heading03: Font = .custom("Pretendard-SemiBold", size: 26)
+    static let Heading04: Font = .custom("Pretendard-Medium", size: 26)
+    static let Heading05: Font = .custom("Pretendard-SemiBold", size: 24)
+    static let Heading06: Font = .custom("Pretendard-Medium", size: 24)
+    static let Heading07: Font = .custom("Pretendard-SemiBold", size: 22)
+    static let Heading08: Font = .custom("Pretendard-Medium", size: 22)
+    static let Heading09: Font = .custom("Pretendard-SemiBold", size: 20)
+    static let Heading10: Font = .custom("Pretendard-Medium", size: 20)
+    
+    // Body
+    static let Body01: Font = .custom("Pretendard-SemiBold", size: 18)
+    static let Body02: Font = .custom("Pretendard-Medium", size: 18)
+    static let Body03: Font = .custom("Pretendard-SemiBold", size: 17)
+    static let Body04: Font = .custom("Pretendard-Medium", size: 17)
+    static let Body05: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let Body06: Font = .custom("Pretendard-Medium", size: 16)
+    static let Body07: Font = .custom("Pretendard-SemiBold", size: 15)
+    static let Body08: Font = .custom("Pretendard-Medium", size: 15)
+    static let Body09: Font = .custom("Pretendard-SemiBold", size: 14)
+    static let Body10: Font = .custom("Pretendard-Medium", size: 14)
+    
+    // Caption
+    static let Caption01: Font = .custom("Pretendard-SemiBold", size: 13)
+    static let Caption02: Font = .custom("Pretendard-Medium", size: 13)
+    static let Caption03: Font = .custom("Pretendard-SemiBold", size: 12)
+    static let Caption04: Font = .custom("Pretendard-Medium", size: 12)
+    static let Caption05: Font = .custom("Pretendard-SemiBold", size: 11)
+    static let Caption06: Font = .custom("Pretendard-Medium", size: 11)
+    
+    // English
+    static let English01: Font = .custom("SUIT-Bold", size: 20)
+    static let English02: Font = .custom("SUIT-Medium", size: 20)
+    static let English03: Font = .custom("SUIT-Bold", size: 18)
+    static let English04: Font = .custom("SUIT-Medium", size: 18)
+    static let English05: Font = .custom("SUIT-Bold", size: 17)
+    static let English06: Font = .custom("SUIT-Medium", size: 17)
+    static let English07: Font = .custom("SUIT-Bold", size: 16)
+    static let English08: Font = .custom("SUIT-Medium", size: 16)
+    static let English09: Font = .custom("SUIT-Bold", size: 15)
+    static let English10: Font = .custom("SUIT-Medium", size: 15)
+    static let English11: Font = .custom("SUIT-Bold", size: 14)
+    static let English12: Font = .custom("SUIT-Medium", size: 14)
+    static let English13: Font = .custom("SUIT-Bold", size: 13)
+    static let English14: Font = .custom("SUIT-Medium", size: 13)
+    static let English15: Font = .custom("SUIT-Bold", size: 12)
+    static let English16: Font = .custom("SUIT-Medium", size: 12)
+    static let English17: Font = .custom("SUIT-Bold", size: 10)
+    static let English18: Font = .custom("SUIT-Medium", size: 10)
+    
+}
---
+++ Spon-us/Spon-us/Spon_usApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spon_usApp.swift
+//  Spon-us
+//
+//  Created by yubin on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Spon_usApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

3c4ecedb65789fda3f0722b82fc236ab15227dc4
insung0927 <78968690+Fiddich-Dev@users.noreply.github.com>
2024-01-09 12:46:57 +0900

    [#12] 내공고 보기 작업중

--- Spon-us/ProposalReceivedListView.swift
+++ Spon-us/ProposalReceivedListView.swift
@@ -7,72 +7,64 @@
 
 import SwiftUI
 
+
+struct Item:Identifiable {
+    var id = UUID()
+    let color: Color
+    let text: String
+}
+
+
+
 struct ProposalReceivedListView: View {
     
     
     
     var body: some View {
-        // Korean/Body/Body06
-        //        Text("현재 받은 제안이 없습니다")
-        //            .font(.Body06)
-        //          .foregroundColor(Constants.sponusGrey900) //색깔 수정
         
-        
-        
-        VStack(spacing: 0){
+        NavigationView {
             
             ScrollView{
-                Rectangle()
-                    .foregroundColor(.blue)
-                    .frame(width: 335, height: 56)
-                //          .background(Constants.sponusSecondary)
-                    .overlay(
-                        Text("확인하지 않은 제안이 2건 있습니다")
-                            .font(.Body06)
-                        //                      .foregroundColor(Constants.sponusBlack)
-                    )
                 
-                Text("12.24 SUN")
-                    .font(.English01)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                //              .foregroundColor(Constants.sponusBlack)
-                
-                Rectangle()
-                    .frame(maxWidth: .infinity, maxHeight: 1)
-                //                    .frame(width: 335, height: 1)
-                //              .background(Constants.sponusBlack)
-                
-//                ProposalReceivedCell()
-                
-                HStack(spacing: 0){
-                    Image("ic_sponus")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-//                        .frame(width: 158, height: 158)
-                        .padding(.trailing, 20)
+                VStack(spacing: 0){
                     
-//                    ZStack{
-//                        Rectangle()
-////                            .frame(width: 158, height: 158)
-//                            .padding(.trailing, 20)
-//                            .foregroundColor(.clear)
-                        
-                    LazyVStack(alignment: .leading){
-                            // Korean/Caption/Caption02
-                            Text("협찬")
-                                .font(.Caption02)
-            //                  .foregroundColor(Constants.sponusGrey700)
-            //                  .frame(width: 137, alignment: .topLeading)
-                        }.padding(.trailing, 20)
-//                    }
+                    
+                    Rectangle()
+                        .fill(Color.sponusSecondary)
+                        .frame(width: 335, height: 56)
+                        .overlay(
+                            Text("확인하지 않은 제안이 ")
+                            +
+                            Text("2건 ")
+                                .foregroundColor(Color.sponusPrimary)
+                            +
+                            Text("있습니다")
+                        )
+                        .font(.Body06)
+                        .foregroundColor(Color.sponusBlack)
+                    
+                    
+                    Text("12.24 SUN")
+                        .font(.English01)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(Color.sponusBlack)
+                        .padding(.top, 32)
+                    
+                    Rectangle()
+                        .fill(Color.sponusBlack)
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .padding(.top, 8)
+                    
+                    ForEach(1 ..< 5) { item in
+                        ProposalReceivedCell()
+                    }
+                    
+                    
                     
                     
                 }
-                .padding(.trailing, 20)
-                
+                .padding(.horizontal, 20)
             }
-            .padding(.horizontal, 20)
-            
         }
     }
 }
@@ -80,24 +72,216 @@ struct ProposalReceivedListView: View {
 
 struct ProposalReceivedCell: View {
     var body: some View {
-        HStack{
+        HStack(spacing: 0){
             Image("ic_sponus")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 158, height: 158)
+            //                        .frame(width: 158, height: 158)
                 .padding(.trailing, 20)
             
-            VStack{
-                // Korean/Caption/Caption02
+            
+            
+            LazyVStack(alignment: .leading, spacing: 12){
                 Text("협찬")
                     .font(.Caption02)
-//                  .foregroundColor(Constants.sponusGrey700)
-//                  .frame(width: 137, alignment: .topLeading)
-            }
-            
-            
+                    .foregroundColor(Color.sponusGrey700)
+                
+                
+                Text("2024 스포대학교\n대동제 협찬")
+                    .font(.Body07)
+                    .foregroundColor(Color.sponusBlack)
+                
+                
+                
+                
+                
+                NavigationLink(destination: {
+                    EmptyView()
+                }, label: {
+                    HStack{
+                        Text("받은 제안서")
+                        
+                        
+                        Image("ic_go")
+                            .frame(width: 16, height: 16)
+                    }
+                    .foregroundStyle(Color.sponusPrimary)
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 10)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color.sponusPrimary, lineWidth: 1)
+                    )
+                })
+                
+                
+                
+                
+            }.padding(.trailing, 20)
         }
         .padding(.trailing, 20)
+        .padding(.top, 16)
+    }
+}
+
+struct MyNoticeView: View {
+    
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 0){
+                ScrollView(.horizontal) {
+                    LazyHStack {
+                        ForEach(0 ..< 5) { trip in
+                            
+                            Image("TestPhoto")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: 300)
+                                .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                                .padding(.horizontal, 20)
+                                .containerRelativeFrame(.horizontal)
+                                .scrollTransition(.animated, axis: .horizontal) { content, phase in
+                                    content
+                                        .opacity(phase.isIdentity ? 1.0 : 0.8)
+                                        .scaleEffect(phase.isIdentity ? 1.0 : 0.8)
+                                }
+                            
+                        }
+                    }
+                    .scrollTargetLayout()
+                    
+                }
+                .scrollTargetBehavior(.viewAligned)
+                
+                
+                HStack{
+                    Image("ic_eye_small")
+                    Text("989")
+                    Image("ic_saved_small")
+                    Text("34")
+                }
+                .font(.English16)
+                .foregroundColor(Color.sponusGrey700)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                LazyVStack(alignment: .leading, spacing: 0) {
+                    HStack{
+                        Text("스포대학교 총학생회")
+                            .font(.Body10)
+                            .foregroundColor(Color.sponusPrimary)
+                        
+                        Image("ic_go")
+                            .frame(width: 16, height: 16)
+                    }
+                    .padding(.top, 23)
+                    // Korean/Heading/Heading05
+                    Text("2024 스포대학교\n대동제 협찬")
+                        .font(.Heading05)
+                        .foregroundColor(Color.sponusBlack)
+                        .padding(.top, 16)
+                    
+                    Rectangle()
+                        .fill(Color.sponusGrey200)
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .padding(.top, 16)
+                    HStack(spacing: 0){
+                        HStack{
+                            Text("유형")
+                                .font(.Body10)
+                                .foregroundColor(Color.sponusGrey700)
+                            Spacer()
+                            
+                            Text("협찬")
+                                .font(.Body10)
+                                .foregroundColor(Color.sponusBlack)
+                                .padding(.trailing, 16)
+                        }
+                        .frame(maxWidth: .infinity)
+                        Rectangle()
+                            .fill(Color.sponusGrey200)
+                            .frame(maxWidth: 1, minHeight: 49)
+                        HStack{
+                            Text("분야")
+                                .font(.Body10)
+                                .foregroundColor(Color.sponusGrey700)
+                                .padding(.leading, 16)
+                            Spacer()
+                            Text("기획/아이디어")
+                                .font(.Body10)
+                                .foregroundColor(Color.sponusBlack)
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                    Rectangle()
+                        .fill(Color.sponusGrey200)
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                    
+                    
+                    // Korean/Body/Body10
+                    Text("공고 상세")
+                        .font(.Body10)
+                        .foregroundColor(Color.sponusGrey700)
+                        .padding(.top, 24)
+                    
+                    // Korean/Body/Body10
+                    Text("안녕하세요 스포대학교 제 21대 학생회 스포너스입니다. 2023년도 스포대학교 대동제에 물품 협찬해 주실 기업은 연락 부탁 드립니다. \n저희 스포대학교는 학생수 2000명의 종합대학으로, 대학생에게 알리 고 싶은 제품이나 기업을 홍보하기에 적합할 것이라 생각합니다. \n대동제 기간: 2023년 9월 20일~ 9월 22일 활동 내용: 카드뉴스 제작, 배너 제작, 기업 인스타그램 태그 이벤트  그 외의 내용은 상세 협의 후 정하고 싶습니다. ")
+                        .font(.Body10)
+                        .foregroundColor(.black)
+                        .padding(.top, 24)
+                    
+                    Rectangle()
+                        .fill(Color.sponusGrey200)
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .padding(.top, 24)
+                    
+                }
+            }
+            .padding(.horizontal, 20)
+            
+        }
+    }
+}
+
+struct MyNoticeView2: View {
+    
+    @State private var scrollEffectValue: Double = 13
+    @State private var activePageIndex: Int = 0
+    
+    let tileWidth: CGFloat = 300
+    let tilePadding: CGFloat = 20
+    let numberOfTiles: Int = 5
+    
+    
+    var body: some View {
+        PagingScrollView(activePageIndex: $activePageIndex, tileWidth: tileWidth, tilePadding: tilePadding) {
+            ForEach(0..<numberOfTiles, id: \.self) { index in
+                GeometryReader { geometry2 in
+                    TileView(icon: "\(index + 1).circle")
+                        .scaleEffect(activePageIndex == index ? 1.0 : 0.8) // Adjust the scale factor
+                        .onTapGesture {
+                            print("tap on index: \(index)")
+                        }
+                }
+            }
+        }
+        .frame(height: 128)
+    }
+}
+
+struct TileView: View {
+    let icon: String
+    
+    var body: some View {
+        //        RoundedRectangle(cornerRadius: 20).fill(Color.gray)
+        //            .overlay(Image(icon).imageScale(.large))
+        //            .frame(height: 128)
+        Image("TestPhoto")
+            .resizable()
+            .scaledToFill()
+            .frame(height: 300)
+            .clipShape(RoundedRectangle(cornerRadius: 25.0))
+            .padding(.horizontal, 20)
     }
 }
 
@@ -107,7 +291,13 @@ struct ProposalReceivedCell: View {
 }
 
 #Preview {
-    ProposalReceivedCell()
+    MyNoticeView()
+    
 }
+//#Preview {
+//    MyNoticeView2()
+//
+//}
+
 
 

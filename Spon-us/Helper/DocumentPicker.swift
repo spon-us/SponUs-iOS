b14a5ca1f1bb5de34a6e0b6d1316f27579973a9e
00yhsp <00yhsp@naver.com>
2024-01-21 21:41:19 +0900

    [#37] 파일 선택 구현 | 미리보기, 보고서 UI 구현

---
+++ Spon-us/Helper/DocumentPicker.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentPicker.swift
+//  Spon-us
+//
+//  Created by 박현수 on 1/19/24.
+//
+
+import SwiftUI
+
+struct DocumentPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentPicker()
+}

42a26d30ee10b6f07d5724a40c4567a1d615b885
00yhsp <00yhsp@naver.com>
2024-01-27 16:49:14 +0900

    [#57]  ~ 로그인 화면 UI 구현

---
+++ Spon-us/View/Onboarding/TermsView.swift
@@ -0,0 +1,18 @@
+//
+//  TermsView.swift
+//  Spon-us
+//
+//  Created by 박현수 on 1/27/24.
+//
+
+import SwiftUI
+
+struct TermsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermsView()
+}

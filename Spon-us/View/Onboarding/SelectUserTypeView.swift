59674cc349fa012f8ec95d266428c0d27842ec57
00yhsp <00yhsp@naver.com>
2024-02-03 22:30:18 +0900

    [#57] 유저 유형 선택 구현중

--- Spon-us/View/Onboarding/SelectUserTypeView.swift
+++ Spon-us/View/Onboarding/SelectUserTypeView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct SelectUserTypeView: View {
+    @State var isStudent = false
+    @State var isCompany = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            Rectangle().frame(height: 1).foregroundStyle(.sponusBlack)
+            HStack(spacing: 0) {
+                Text("어느 단체이신가요?")
+                    .font(.Heading06)
+                    .lineSpacing(6)
+                    .padding(.top, 28)
+                Spacer()
+            }
+            HStack(spacing: 0) {
+                Button {
+                    isStudent.toggle()
+                    isCompany = false
+                } label: {
+                    Image(isStudent ? .icCircleFilled : .icCircle).resizable().frame(width: 20, height: 20)
+                }.padding(.leading, 16)
+                Text("대학생 단체입니다").font(.Body06).padding(.leading, 16)
+                Spacer()
+            }.frame(height: 56).border(isStudent ? .sponusPrimary : .sponusGrey100).padding(.top, 68)
+            HStack(spacing: 0) {
+                Button {
+                    isCompany.toggle()
+                    isStudent = false
+                } label: {
+                    Image(isCompany ? .icCircleFilled : .icCircle).resizable().frame(width: 20, height: 20)
+                }.padding(.leading, 16)
+                Text("기업 담당자입니다").font(.Body06).padding(.leading, 16)
+                Spacer()
+            }.frame(height: 56).border(isCompany ? .sponusPrimary : .sponusGrey100).padding(.top, 16)
+            Spacer()
+            NavigationLink {
+                SelectUserTypeView()
+            } label: {
+                Text("다음").font(.Body04).frame(maxWidth: .infinity).frame(height: 56).foregroundStyle(.sponusWhite).background(!(isStudent || isCompany) ? .sponusGrey600 : .sponusPrimary).padding(.bottom, 16)
+            }.disabled(!(isStudent || isCompany))
+        }.padding(.horizontal, 20)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: CustomBackButton())
     }
 }
 

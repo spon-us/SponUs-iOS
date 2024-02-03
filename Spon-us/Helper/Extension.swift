28377e5fe6694ac373bc2496dbd949c97acb4dae
yubin <yubin412@naver.com>
2024-02-03 21:20:45 +0900

    [#63] 마이페이지 전체적인 UI 구현
    - 문의하기 카카오 오픈채팅방 링크로 연결
    - 로그아웃 / 계정탈퇴 Alert까지 구현

--- Spon-us/Helper/Extension.swift
+++ Spon-us/Helper/Extension.swift
@@ -14,35 +14,39 @@ extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate 
         super.viewDidLoad()
         interactivePopGestureRecognizer?.delegate = self
     }
-
+    
     public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         return viewControllers.count > 1
     }
 }
 
 extension UIScreen{
-   static let screenWidth = UIScreen.main.bounds.size.width
-   static let screenHeight = UIScreen.main.bounds.size.height
-   static let screenSize = UIScreen.main.bounds.size
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+    static let screenSize = UIScreen.main.bounds.size
 }
 
 
 extension View {
     func navigationBarBackground(_ background: Color = Color(.sponusBlack).opacity(0.6)) -> some View {
-    return self
-      .modifier(ColoredNavigationBar(background: background))
-  }
+        return self
+            .modifier(ColoredNavigationBar(background: background))
+    }
+    func mynavigationBarBackground(_ background: Color = Color(.sponusBlack)) -> some View {
+        return self
+            .modifier(ColoredNavigationBar(background: background))
+    }
 }
 
 struct ColoredNavigationBar: ViewModifier {
-  var background: Color
-  
-  func body(content: Content) -> some View {
-    content
-      .toolbarBackground(
-        background,
-        for: .navigationBar
-      )
-      .toolbarBackground(.visible, for: .navigationBar)
-  }
+    var background: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .toolbarBackground(
+                background,
+                for: .navigationBar
+            )
+            .toolbarBackground(.visible, for: .navigationBar)
+    }
 }
--- Spon-us/View/MyPage/MyView.swift
+++ Spon-us/View/MyPage/MyView.swift
@@ -8,12 +8,209 @@
 import SwiftUI
 
 struct MyView: View {
+    @State private var showLogoutAlert = false
+    @State private var showAlert = false
+    
+    let searchItem: some View = {
+        NavigationLink {
+            SearchView()
+        } label: {
+            Image("ic_search_white")
+        }
+    }()
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("마이페이지")
-                    .navigationTitle("마이페이지")
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("MY")
+                    .font(
+                        Font.custom("SUIT", size: 30)
+                            .weight(.bold)
+                    )
+                    .foregroundColor(Color.sponusWhite)
+                
+                HStack(spacing: 0) {
+                    Image("profile_test")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        NavigationLink(destination: EmptyView(), label: {
+                            HStack {
+                                Text("Profile")
+                                    .font(.English15)
+                                    .foregroundColor(Color.sponusPrimary)
+                                
+                                Image("ic_go_12px")
+                                    .frame(width: 12, height: 12)
+                                    .padding(.leading, -6)
+                            }
+                        })
+                        .padding(.leading, 12)
+                        .padding(.trailing, 7)
+                        .padding(.vertical, 4)
+                        .frame(width: 69, height: 26, alignment: .center)
+                        .background(Color.sponusSecondary)
+                        .cornerRadius(99)
+                        
+                        Text("스포대학교 총학생회")
+                            .font(.Heading09)
+                            .foregroundColor(Color.sponusWhite)
+                            .padding(.top, 11)
+                    }
+                    .padding(.leading, 16)
+                    
+                }
+                .padding(.top, 24)
+                .padding(.bottom, 36)
             }
+            .padding(.leading, 20)
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            .frame(width: UIScreen.screenWidth, height: 205)
+            .background(Color.sponusBlack)
+            
+            ScrollView {
+                VStack(spacing: 20) {
+                    NavigationLink(destination: EmptyView()) {
+                        HStack {
+                            Text("담당자 정보 수정하기")
+                                .font(.Body05)
+                                .foregroundColor(Color.sponusBlack)
+                            Spacer()
+                            
+                            Image("ic_move_grey").frame(width: 28, height: 28)
+                        }
+                    }.frame(height: 28)
+                    
+                    Divider()
+                    
+                    NavigationLink(destination: EmptyView()) {
+                        HStack {
+                            Text("쿠폰함")
+                                .font(.Body05)
+                                .foregroundColor(Color.sponusBlack)
+                            Spacer()
+                            
+                            Image("ic_move_grey").frame(width: 28, height: 28)
+                        }
+                    }.frame(height: 28)
+                    
+                    Divider()
+                    
+                    Link(destination: URL(string: "http://pf.kakao.com/_QxkxfSG")!) {
+                        HStack {
+                            Text("문의하기")
+                                .font(.Body05)
+                                .foregroundColor(Color.sponusBlack)
+                            Spacer()
+                            
+                            Image("ic_move_grey").frame(width: 28, height: 28)
+                        }
+                    }.frame(height: 28)
+                    
+                    Divider()
+                    
+                    NavigationLink(destination: EmptyView()) {
+                        HStack {
+                            Text("개인정보처리방침")
+                                .font(.Body05)
+                                .foregroundColor(Color.sponusBlack)
+                            Spacer()
+                            
+                            Image("ic_move_grey").frame(width: 28, height: 28)
+                        }
+                    }.frame(height: 28)
+                    
+                    Divider()
+                    
+                    NavigationLink(destination: EmptyView()) {
+                        HStack {
+                            Text("이용약관")
+                                .font(.Body05)
+                                .foregroundColor(Color.sponusBlack)
+                            Spacer()
+                            
+                            Image("ic_move_grey").frame(width: 28, height: 28)
+                        }
+                    }.frame(height: 28)
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("버전정보")
+                            .font(.Body05)
+                            .foregroundColor(Color.sponusBlack)
+                        Spacer()
+                        
+                        Text("1.1.1")
+                            .font(.Body10)
+                            .foregroundColor(Color.sponusGrey600)
+                            .padding(.trailing, 12)
+                    }.frame(height: 28)
+                    
+                    Divider()
+                    
+                    Button(action: {
+                        showLogoutAlert = true
+                    }) {
+                        Text("로그아웃")
+                            .font(.Body10)
+                            .foregroundColor(Color.sponusBlack)
+                            .padding(.horizontal, 12)
+                            .padding(.vertical, 10)
+                            .overlay(
+                                Rectangle()
+                                    .inset(by: 0.5)
+                                    .stroke(Color.sponusBlack, lineWidth: 1))
+                    }
+                    .padding(.top, 40)
+                    .alert(isPresented: $showLogoutAlert) {
+                        Alert(
+                            title: Text("로그아웃 하시겠습니까?"),
+                            primaryButton: .destructive(
+                                Text("아니오")
+                            ),
+                            secondaryButton: .default(
+                                Text("로그아웃"),
+                                action: {}
+                            )
+                        )
+                    }
+                    
+                    Button(action: {
+                        showAlert = true
+                    }) {
+                        Text("계정탈퇴")
+                            .font(.Caption04)
+                            .foregroundColor(Color.sponusGrey600)
+                    }
+                    .alert(isPresented: $showAlert) {
+                        Alert(
+                            title: Text("계정 탈퇴를 진행하시겠습니까?"),
+                            message: Text("탈퇴 이후에는 되돌리기 어렵습니다."),
+                            primaryButton: .default(
+                                Text("취소")
+                            ),
+                            secondaryButton: .destructive(
+                                Text("탈퇴"),
+                                action: {}
+                            )
+                        )
+                    }
+                }
+                .padding(.top, 35)
+                .padding([.leading, .trailing], 20)
+                
+            }
+            .padding(.bottom, 34)
+            .navigationBarItems(trailing: searchItem)
+            .mynavigationBarBackground()
         }
     }
+}
+
+#Preview {
+    MyView()
 }

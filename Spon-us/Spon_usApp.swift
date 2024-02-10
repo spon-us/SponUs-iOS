a64722e6bc3315a427cee32d9634edb7963e81c2
kimsoomin <skno1382@soongsil.ac.kr>
2024-02-11 03:12:47 +0900

    [#81] FCM 푸쉬알림 초기 설정 구현
    - 토큰 콘솔에 찍히도록 (서버통신 x)

--- Spon-us/Spon_usApp.swift
+++ Spon-us/Spon_usApp.swift
@@ -6,17 +6,77 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import UserNotifications
+import FirebaseMessaging
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // 파이어베이스 설정
+        FirebaseApp.configure()
+        
+        // 앱 실행 시 사용자에게 알림 허용 권한을 받음
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound] // 필요한 알림 권한을 설정
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        // UNUserNotificationCenterDelegate를 구현한 메서드를 실행시킴
+        application.registerForRemoteNotifications()
+        
+        // 파이어베이스 Meesaging 설정
+        Messaging.messaging().delegate = self
+        
+        return true
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    // 백그라운드에서 푸시 알림을 탭했을 때 실행
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS token: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // Foreground(앱 켜진 상태)에서도 알림 오는 설정
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    // 파이어베이스 MessagingDelegate 설정
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+}
+
 
 @main
 struct Spon_usApp: App {
-    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @State var rootIsActive = true
-    
     var body: some Scene {
         WindowGroup {
             OnBoardingView()
-//            ContentView()
-//            ChargerInfoViewTest(rootIsActive: $rootIsActive)
+            //            ContentView()
+            //            ChargerInfoViewTest(rootIsActive: $rootIsActive)
         }
     }
 }

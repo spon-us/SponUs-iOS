80ce0d1eb1713d026f7b0153c49be965e75073fb
00yhsp <00yhsp@naver.com>
2024-02-12 11:38:44 +0900

    [#73] 로그인 API연동 완료 | 자동로그인, 키체인 토큰 저장 구현중

--- Spon-us/Model/Onboarding/LoginViewModel.swift
+++ Spon-us/Model/Onboarding/LoginViewModel.swift
@@ -9,23 +9,28 @@ import Foundation
 import Moya
 
 class LoginViewModel: ObservableObject {
-    @Published var login200: LoginModel200?
+    @Published var login201: LoginModel201?
     @Published var login401: LoginModel401?
+    @Published var isBadRequest = false
     
     private let provider = MoyaProvider<SponusAPI>(plugins: [NetworkLoggerPlugin()])
     
-    func postLogin(email: String, password: String, fcmToken: String) {
+    func postLogin(email: String, password: String, fcmToken: String, completion: @escaping (Bool) -> Void) {
         provider.request(.postLogin(email: email, password: password, fcmToken: fcmToken)) { result in
             switch result {
             case let .success(response):
                 do {
-                    if response.statusCode == 200 {
-                        let loginResponse = try response.map(LoginModel200.self)
-                        self.login200 = loginResponse
+                    if response.statusCode == 201 {
+                        let loginResponse = try response.map(LoginModel201.self)
+                        self.login201 = loginResponse
+                        self.isBadRequest = false
+                        completion(true)
                     }
                     else {
                         let loginResponse = try response.map(LoginModel401.self)
                         self.login401 = loginResponse
+                        self.isBadRequest = true
+                        completion(false)
                     }
                 } catch {
                     print("Error parsing response: \(error)")

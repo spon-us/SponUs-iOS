aa08c4ddbe84625ea7e34c92318db6a2ed4df4ff
yubin <yubin412@naver.com>
2024-02-18 15:27:48 +0900

    [#123] 보낸 제안 이미지 연결

--- Spon-us/View/SendOffer/SendOfferPostView.swift
+++ Spon-us/View/SendOffer/SendOfferPostView.swift
@@ -21,73 +21,55 @@ struct SendOfferPostView: View {
     var body: some View {
         VStack(spacing: 0) {
             ScrollView {
-                LazyVStack(spacing: 0) {
-                    ScrollView(.horizontal) {
-                        LazyHStack(spacing: 10) {
-                            ForEach(1...3, id: \.self) { index in
-                                Image("post\(index)")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 335, height: 335)
-                                    .containerRelativeFrame(.horizontal)
-                                    .scrollTransition(.animated, axis: .horizontal) { content, phase in
-                                        content
-                                            .opacity(phase.isIdentity ? 1.0 : 0.8)
-                                            .scaleEffect(phase.isIdentity ? 1.0 : 0.8)
-                                    }
-                            }
-                        }
-                        .scrollTargetLayout()
-                    }
-                    .scrollTargetBehavior(.viewAligned)
-                    .safeAreaPadding(.horizontal, 40.0)
+                VStack(alignment: .leading, spacing: 0) {
+                    AsyncImageView(url: URL(string: proposalDetailViewModel.proposalDetail?.proposingOrganizationImage ?? "profile_test"))
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 335, height: 335)
+                        .clipped()
                     
-                    VStack(alignment: .leading, spacing: 0) {
-                        NavigationLink {
-                            ProfileView(rootIsActive: $rootIsActive)
-                        } label: {
-                            HStack {
-                                Text(proposalDetailViewModel.proposalDetail?.proposedOrganizationName ?? "")
-                                    .font(.Body10)
-                                    .foregroundColor(Color.sponusPrimary)
-                                
-                                Image("ic_go_blue")
-                                    .frame(width: 16, height: 16)
-                            }
-                        }.padding(.top, 23)
-                        
-                        Text(proposalDetailViewModel.proposalDetail?.title ?? "")
-                            .font(.Heading05)
-                            .foregroundColor(Color.sponusBlack)
-                            .padding(.top, 16)
-                        
-                        Rectangle()
-                            .fill(Color.sponusGrey200)
-                            .frame(maxWidth: .infinity, maxHeight: 1)
-                            .padding(.top, 16)
-                        
-                        Text("제안 상세")
-                            .font(.Body10)
-                            .foregroundColor(Color.sponusGrey700)
-                            .padding(.top, 24)
-                        
-                        Text(proposalDetailViewModel.proposalDetail?.content ?? "")
-                            .multilineTextAlignment(.leading)
-                            .font(.Body10)
-                            .foregroundColor(.black)
-                            .padding(.top, 14)
-                        
-                        Rectangle()
-                            .fill(Color.sponusGrey200)
-                            .frame(maxWidth: .infinity, maxHeight: 1)
-                            .padding(.top, 24)
-                        
-                        SendOfferPostCell(rootIsActive: $rootIsActive, status: statusChangeToKorean(english: proposalDetailViewModel.proposalDetail?.announcementDetails.status ?? ""), cellData: proposalDetailViewModel.proposalDetail?.announcementDetails ?? AnnouncementDetails())
-                            .padding(.vertical, 16)
-                    }
-                    .padding(.horizontal, 20)
+                    NavigationLink {
+                        ProfileView(rootIsActive: $rootIsActive)
+                    } label: {
+                        HStack {
+                            Text(proposalDetailViewModel.proposalDetail?.proposedOrganizationName ?? "")
+                                .font(.Body10)
+                                .foregroundColor(Color.sponusPrimary)
+                            
+                            Image("ic_go_blue")
+                                .frame(width: 16, height: 16)
+                        }
+                    }.padding(.top, 23)
+                    
+                    Text(proposalDetailViewModel.proposalDetail?.title ?? "")
+                        .font(.Heading05)
+                        .foregroundColor(Color.sponusBlack)
+                        .padding(.top, 16)
+                    
+                    Rectangle()
+                        .fill(Color.sponusGrey200)
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .padding(.top, 16)
+                    
+                    Text("제안 상세")
+                        .font(.Body10)
+                        .foregroundColor(Color.sponusGrey700)
+                        .padding(.top, 24)
+                    
+                    Text(proposalDetailViewModel.proposalDetail?.content ?? "")
+                        .multilineTextAlignment(.leading)
+                        .font(.Body10)
+                        .foregroundColor(.black)
+                        .padding(.top, 14)
+                    
+                    Rectangle()
+                        .fill(Color.sponusGrey200)
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .padding(.top, 24)
+                    
+                    SendOfferPostCell(rootIsActive: $rootIsActive, status: statusChangeToKorean(english: proposalDetailViewModel.proposalDetail?.announcementDetails.status ?? ""), cellData: proposalDetailViewModel.proposalDetail?.announcementDetails ?? AnnouncementDetails())
+                        .padding(.vertical, 16)
                 }
-                .padding(.bottom, 20)
+                .padding(.all, 20)
             }
             
             HStack() {
@@ -142,24 +124,14 @@ struct SendOfferPostCell: View {
     
     var body: some View {
         HStack(spacing: 0) {
-            /*
-            ZStack {
-                Image("musinsa")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
+            ForEach(cellData.announcementImages, id: \.self) { image in
+                AsyncImageView(url: URL(string: image.url))
+                    .aspectRatio(contentMode: .fill)
                     .frame(width: 158, height: 158)
+                    .clipped()
                     .padding(.trailing, 20)
+            }
                 
-                StatusBadge(status: statusChangeToKorean(english: cellData.status))
-                    .offset(x: 50.5, y: -66.5)
-            }*/
-            
-            Image("musinsa")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 158, height: 158)
-                .padding(.trailing, 20)
-            
             VStack(alignment: .leading, spacing: 12) {
                 Text("\(changeToKorean(type: cellData.type) ?? "전체")")
                     .font(.Caption02)

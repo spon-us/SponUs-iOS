11e39cd625e2242fed9db4b666ec238c4e7f8f2b
00yhsp <00yhsp@naver.com>
2024-02-19 01:52:35 +0900

    누락 뷰 파일 추가

--- Spon-us/View/ProfileView.swift
+++ Spon-us/View/ProfileView.swift
@@ -1,71 +1,87 @@
 //
-//  ProfileVIew.swift
-//  Spon-us
+// ProfileVIew.swift
+// Spon-us
 //
-//  Created by 황인성 on 2024/01/16.
+// Created by 황인성 on 2024/01/16.
 //
-
 import SwiftUI
-
 struct ProfileView: View {
     @Binding var rootIsActive: Bool
     var organizationId: Int
-    @ObservedObject var organizationViewModel: OrganizationViewModel = OrganizationViewModel()
+    @StateObject var organizationViewModel: OrganizationViewModel = OrganizationViewModel()
     var body: some View {
         VStack(spacing: 0) {
-            
             ScrollView {
-
-                    Image("TestImage")
-                        .resizable()
+                ZStack(alignment: .bottomLeading) {
+                    AsyncImageView(url: URL(string: organizationViewModel.organization?.imageUrl ?? "profile_test"))
                         .aspectRatio(contentMode: .fill)
                         .frame(maxWidth: .infinity, maxHeight: 540)
+                        .clipped()
                         .padding(.bottom, 40)
-
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(maxWidth: .infinity).frame(height: 178)
+                        .background(
+                            LinearGradient(
+                                stops: [
+                                    Gradient.Stop(color: Color(red: 0.13, green: 0.13, blue: 0.13).opacity(0), location: 0.00),
+                                    Gradient.Stop(color: Color(red: 0.13, green: 0.13, blue: 0.13).opacity(0.45), location: 0.55),
+                                    Gradient.Stop(color: Color(red: 0.13, green: 0.13, blue: 0.13).opacity(0.9), location: 1.00),
+                                ],
+                                startPoint: UnitPoint(x: 0.5, y: 0),
+                                endPoint: UnitPoint(x: 0.5, y: 1)
+                            )
+                        )
+                    VStack(alignment:.leading, spacing: 8) {
+                        Text(organizationViewModel.organization?.suborganizationType ?? "")
+                            .font(.English15)
+                            .foregroundColor(Color.sponusPrimary)
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 4)
+                            .background(
+                                RoundedRectangle(cornerRadius: 99)
+                                    .foregroundColor(Color.sponusSecondary)
+                            )
+                        Text(organizationViewModel.organization?.name ?? "")
+                            .font(.Heading01)
+                            .foregroundColor(Color.sponusWhite)
+                            .padding(.bottom, 25)
+                    }
+                    .padding(.leading, 40)
+                }
                 VStack(alignment: .leading, spacing: 16){
-                    
-                    Text(organizationViewModel.organization?.name ?? "안불러와짐.")
+                    Text("단체 소개")
                         .font(.Heading09)
-                    
+                        .padding(.top, 40)
                     SponUsDivider()
                         .padding(.bottom, 8)
-                    
-                    Text(organizationViewModel.organization?.description ?? "안불러와짐.")
+                    Text(organizationViewModel.organization?.description ?? "")
                         .multilineTextAlignment(.leading)
                         .font(.Body10)
-                      .foregroundColor(Color.sponusGrey800)
-                      .padding(.trailing, 20)
-                      .padding(.bottom, 24)
-                    
-                    Text("기업 정보")
+                        .foregroundColor(Color.sponusGrey800)
+                        .padding(.trailing, 20)
+                        .padding(.bottom, 24)
+                    Text("단체 정보")
                         .font(.Heading09)
-                    
                     SponUsDivider()
                         .padding(.bottom, 8)
-                    
                     ProfileSNSView()
-                    .padding(.bottom, 24)
-                    
-                    Text("활동 기록")
-                        .font(.Heading09)
-                    
-                    SponUsDivider()
-                        .padding(.bottom, 8)
-                    
-                    ProfileHistoryCell(stateMessage: "게시 중 공고")
-                    .padding(.bottom, 4)
-                    
-                    ProfileHistoryCell(stateMessage: "진행 중 공고")
-                    .padding(.bottom, 4)
-                    
-                    ProfileHistoryCell(stateMessage: "완료된 공고")
-                    
-                    
+                        .padding(.bottom, 24)
+                    /*
+                     Text("활동 기록")
+                     .font(.Heading09)
+                     SponUsDivider()
+                     .padding(.bottom, 8)
+                     ProfileHistoryCell(stateMessage: "게시 중 공고")
+                     .padding(.bottom, 4)
+                     ProfileHistoryCell(stateMessage: "진행 중 공고")
+                     .padding(.bottom, 4)
+                     ProfileHistoryCell(stateMessage: "완료된 공고")
+                     */
                 }
                 .foregroundColor(Color.sponusBlack)
                 .padding(.leading, 40)
             }
-            
         }
         .onAppear(){
             organizationViewModel.fetchOrganization(organizationId: organizationId)
@@ -80,108 +96,91 @@ struct ProfileView: View {
                 .foregroundStyle(.black)
         }))
     }
-    
-    
-    
 }
-
 struct ProfileSNSView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 24) {
-            
             Button(action: {
                 openInstagram() //인스타주소 수정해야됨
             }, label: {
                 HStack(spacing: 13) {
                     Text("Instagram")
                         .font(.English08)
-                    .foregroundColor(Color.sponusGrey800)
-                    
+                        .foregroundColor(Color.sponusGrey800)
                     Image("ic_link")
                 }
             })
-
-          
             Button(action: {
                 openDefaultApp()
             }, label: {
                 HStack(spacing: 13) {
                     Text("musinsa.com")
                         .font(.English08)
-                    .foregroundColor(Color.sponusGrey800)
-                    
+                        .foregroundColor(Color.sponusGrey800)
                     Image("ic_link")
                 }
             })
-            
-            
             Button(action: {
                 openFacebook()
             }, label: {
                 HStack(spacing: 13) {
                     Text("facebook")
                         .font(.English08)
-                    .foregroundColor(Color.sponusGrey800)
-                    
+                        .foregroundColor(Color.sponusGrey800)
                     Image("ic_link")
                 }
             })
         }
     }
-    
-    
     func openInstagram() {
-            let instagramUrl = URL(string: "instagram://user?username=inseong53")!
-            if UIApplication.shared.canOpenURL(instagramUrl) {
-                UIApplication.shared.open(instagramUrl, options: [:], completionHandler: nil)
-            } else {
-                // Instagram 앱이 설치되어 있지 않을 경우, 웹사이트를 열 수 있습니다.
-                let webUrl = URL(string: "https://www.instagram.com/inseong53/")!
-                UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
-            }
+        let instagramUrl = URL(string: "instagram://user?username=sponus_official")!
+        if UIApplication.shared.canOpenURL(instagramUrl) {
+            UIApplication.shared.open(instagramUrl, options: [:], completionHandler: nil)
+        } else {
+            // Instagram 앱이 설치되어 있지 않을 경우, 웹사이트를 열 수 있습니다.
+            let webUrl = URL(string: "https://www.instagram.com/sponus_official/")!
+            UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
         }
-    
+    }
     func openDefaultApp() {
-            guard let url = URL(string: "http://www.musinsa.com") else {
-                return
-            }
-
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        guard let url = URL(string: "http://www.musinsa.com") else {
+            return
         }
-    
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
     func openFacebook() {
-            let facebookUrl = URL(string: "facebook://")!
-            if UIApplication.shared.canOpenURL(facebookUrl) {
-                UIApplication.shared.open(facebookUrl, options: [:], completionHandler: nil)
-            } else {
-                // Instagram 앱이 설치되어 있지 않을 경우, 웹사이트를 열 수 있습니다.
-                let webUrl = URL(string: "https://www.facebook.com/")!
-                UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
-            }
+        let facebookUrl = URL(string: "facebook://")!
+        if UIApplication.shared.canOpenURL(facebookUrl) {
+            UIApplication.shared.open(facebookUrl, options: [:], completionHandler: nil)
+        } else {
+            // Instagram 앱이 설치되어 있지 않을 경우, 웹사이트를 열 수 있습니다.
+            let webUrl = URL(string: "https://www.facebook.com/")!
+            UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
         }
+    }
 }
-
-
 struct ProfileHistoryCell: View {
-    
     var stateMessage: String
-    
     var body: some View {
         HStack {
             Text(stateMessage)
                 .font(.Body06)
-            .foregroundColor(Color.sponusGrey800)
-            
+                .foregroundColor(Color.sponusGrey800)
             Spacer()
-            
             Text("1건")
                 .font(.English07)
-              .foregroundColor(Color.sponusPrimary)
+                .foregroundColor(Color.sponusPrimary)
         }
         .frame(width: 133)
     }
 }
-
 //#Preview {
-//    ProfileView(rootIsActive: .constant(false))
+//  ProfileView(rootIsActive: .constant(false))
 //}
+
+
+
+
+
+
+
--- Spon-us/View/SendOffer/SendOfferPostView.swift
+++ Spon-us/View/SendOffer/SendOfferPostView.swift
@@ -1,23 +1,18 @@
 //
-//  SendOfferPostView.swift
-//  Spon-us
+// SendOfferPostView.swift
+// Spon-us
 //
-//  Created by yubin on 1/21/24.
+// Created by yubin on 1/21/24.
 //
-
 import SwiftUI
 import Moya
-
 struct SendOfferPostView: View {
     var proposeId: Int
     @Binding var rootIsActive: Bool
     @Environment(\.presentationMode) var presentationMode
-    
     @State private var isShowingActivityView = false
     @State private var activityItems: [Any] = [URL(string: "https://example.com")!]
-    
     @ObservedObject var proposalDetailViewModel = ProposalDetailViewModel()
-    
     var body: some View {
         VStack(spacing: 0) {
             ScrollView {
@@ -26,55 +21,46 @@ struct SendOfferPostView: View {
                         .aspectRatio(contentMode: .fill)
                         .frame(height: 335)
                         .clipped()
-                    
-
-                    NavigationLink {
-                        // ProfileView(rootIsActive: $rootIsActive)
-                        Text("더미 채우기")
-                    } label: {
-                        HStack {
-                            Text(proposalDetailViewModel.proposalDetail?.proposedOrganizationName ?? "")
-                                .font(.Body10)
-                                .foregroundColor(Color.sponusPrimary)
-                            
-                            Image("ic_go_blue")
-                                .frame(width: 16, height: 16)
+                    NavigationLink(
+                        destination: ProfileView(rootIsActive: .constant(false), organizationId: proposalDetailViewModel.proposalDetail?.proposingOrganizationId ?? 1),
+                        //isActive: .constant(selectedProposeId == proposalDetailViewModel.proposalDetail?.proposingOrganizationId),
+                        label: {
+                            HStack {
+                                Text(proposalDetailViewModel.proposalDetail?.proposingOrganizationName ?? "")
+                                    .font(.Body10)
+                                    .foregroundColor(Color.sponusPrimary)
+                                Image("ic_go_blue")
+                                    .frame(width: 16, height: 16)
+                            }
+                            .padding(.top, 23)
                         }
-                    }.padding(.top, 23)
-                    
+                    )
                     Text(proposalDetailViewModel.proposalDetail?.title ?? "")
                         .font(.Heading05)
                         .foregroundColor(Color.sponusBlack)
                         .padding(.top, 16)
-                    
                     Rectangle()
                         .fill(Color.sponusGrey200)
                         .frame(maxWidth: .infinity, maxHeight: 1)
                         .padding(.top, 16)
-                    
                     Text("제안 상세")
                         .font(.Body10)
                         .foregroundColor(Color.sponusGrey700)
                         .padding(.top, 24)
-                    
                     Text(proposalDetailViewModel.proposalDetail?.content ?? "")
                         .multilineTextAlignment(.leading)
                         .font(.Body10)
                         .foregroundColor(.black)
                         .padding(.top, 14)
-                    
                     Rectangle()
                         .fill(Color.sponusGrey200)
                         .frame(maxWidth: .infinity, maxHeight: 1)
                         .padding(.top, 24)
-                    
                     SendOfferPostCell(rootIsActive: $rootIsActive, status: statusChangeToKorean(english: proposalDetailViewModel.proposalDetail?.announcementDetails.status ?? ""), cellData: proposalDetailViewModel.proposalDetail?.announcementDetails ?? AnnouncementDetails())
                         .padding(.vertical, 16)
-
                 }
                 .padding(.all, 20)
             }
-            
             HStack() {
                 HStack(spacing: 16) {
                     Button(action: {
@@ -88,7 +74,6 @@ struct SendOfferPostView: View {
                     }
                 }
                 .padding(.leading, 36)
-                
                 NavigationLink(destination: ChargerInfoView(rootIsActive: $rootIsActive)) {
                     Text("담당자 정보 확인하기")
                         .font(.Body01)
@@ -98,6 +83,9 @@ struct SendOfferPostView: View {
                 }
             }
             .background(Color.sponusBlack)
+        }
+        .onAppear(){
+            proposalDetailViewModel.fetchProposalDetail(proposeId: proposeId)
         }
         .gesture(
             DragGesture().onEnded { value in
@@ -114,17 +102,12 @@ struct SendOfferPostView: View {
                 .renderingMode(.template)
                 .foregroundStyle(.black)
         }))
-        .onAppear(){
-            proposalDetailViewModel.fetchProposalDetail(proposeId: proposeId)
-        }
     }
 }
-
 struct SendOfferPostCell: View {
     @Binding var rootIsActive: Bool
     var status: String
     var cellData: AnnouncementDetails
-    
     var body: some View {
         HStack(spacing: 0) {
             ForEach(cellData.announcementImages, id: \.self) { image in
@@ -134,25 +117,20 @@ struct SendOfferPostCell: View {
                     .clipped()
                     .padding(.trailing, 20)
             }
-                
             VStack(alignment: .leading, spacing: 12) {
                 Text("\(changeToKorean(type: cellData.type) ?? "전체")")
                     .font(.Caption02)
                     .foregroundColor(Color.sponusGrey700)
-                
                 Text(cellData.title)
                     .multilineTextAlignment(.leading)
                     .font(.Body07)
                     .foregroundColor(Color.sponusBlack)
-                
                 Spacer()
-                
                 NavigationLink(destination: CompanyPostView(rootIsActive: $rootIsActive)) {
                     HStack {
                         Text("공고 보기")
                             .font(.Body10)
                             .padding(.leading, 5)
-                        
                         Image("ic_go_blue")
                             .frame(width: 16, height: 16)
                             .padding(.leading, -3)
@@ -172,3 +150,15 @@ struct SendOfferPostCell: View {
         .padding(.top, 16)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
